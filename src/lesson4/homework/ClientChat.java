e26a5f33ef07541acde4388d4277860e785451b3
OKrylov <krylov92oa@gmail.com>
2020-03-05 23:51:25 +0300

    hw4

--- src/lesson4/homework/ClientChat.java
+++ src/lesson4/homework/ClientChat.java
@@ -1,18 +1,55 @@
 package lesson4.homework;
 
-import lesson4.swing.TestForm;
-
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ClientChat {
 
-    private JPanel panel1;
-    private JList list1;
-    private JTextArea textArea1;
-    private JTextField textField1;
+    private JPanel mainPanel;
+    private JList<String> usersList;
+    private JTextField messageTextField;
     private JButton sendButton;
+    private JTextArea chatText;
 
+    public ClientChat() {
+        addListeners();
+    }
+
+    private void addListeners() {
+//        sendButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                sendMessage();
+//            }
+//        });
+        sendButton.addActionListener(e -> ClientChat.this.sendMessage());
+        messageTextField.addActionListener(e -> sendMessage());
+    }
+
+    private void sendMessage() {
+        String message = messageTextField.getText().trim();
+        if (message.isEmpty()) {
+            return;
+        }
+
+        appendOwnMessage(message);
+        String selectedUser = usersList.getSelectedValue();
+        if (selectedUser != null) {
+            appendMessage(selectedUser, message);
+        }
+        messageTextField.setText(null);
+    }
+
+    private void appendMessage(String sender, String message) {
+        String formattedMessage = String.format("%s: %s%n", sender, message);
+        chatText.append(formattedMessage);
+    }
+
+
+    private void appendOwnMessage(String message) {
+        appendMessage("Я", message);
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
@@ -20,17 +57,8 @@ public class ClientChat {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(640, 480);
         frame.setLocationRelativeTo(null);
-        frame.setContentPane(new ClientChat().panel1);
+        frame.setContentPane(new ClientChat().mainPanel);
         frame.setVisible(true);
-
-
-    }
-
-    public static void centreWindow(Window frame) {
-        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = (int) ((dimension.getWidth() - frame.getWidth()) / 2);
-        int y = (int) ((dimension.getHeight() - frame.getHeight()) / 2);
-        frame.setLocation(x, y);
     }
 
 }
